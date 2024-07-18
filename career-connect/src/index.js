2893d35fa5e87ec24cf82e2a51bce3c6f5c4db7b
thanhleee <tle58440@gmail.com>
2024-07-18 12:55:24 +0700

    thanhcommit

--- career-connect/src/index.js
+++ career-connect/src/index.js
@@ -1,11 +1,18 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import BrowserRouter và Route
 import './index.css';
 import App from './App';
+import Register from './user/register/Register'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        
+      </Routes>
+    </BrowserRouter>
 );
