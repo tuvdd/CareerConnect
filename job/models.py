80a916876653f07afdc0814a0087c91b4ed8d68e
Vuong Duy Duc Tu <112555576+tuvdd@users.noreply.github.com>
2024-07-22 19:15:21 +0700

    quick fix: change id in job model

--- job/models.py
+++ job/models.py
@@ -7,11 +7,11 @@ class Job(models.Model):
     location = models.CharField(max_length=100)
     salary = models.BigIntegerField()
     status = models.CharField(max_length=100, default="Activated")
-    companyId = models.ForeignKey(Company, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 class Application(models.Model):
     date = models.DateField()
     status = models.CharField(max_length=100)
     resume = models.FileField(upload_to='resumes/')
-    candidateId = models.ForeignKey(Candidate, on_delete=models.CASCADE)
-    jobId = models.ForeignKey(Job, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
