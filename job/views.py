b499e97113b75c3558526bfaf1b3e5b781ae3565
Vuong Duy Duc Tu <112555576+tuvdd@users.noreply.github.com>
2024-07-25 15:06:15 +0700

    feat: search job/application

--- job/views.py
+++ job/views.py
@@ -1,18 +1,46 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from .models import Job, Application
 from .serializers import JobSerializer, ApplicationSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+import django_filters
 
+
+class JobFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains')
+    location = django_filters.CharFilter(lookup_expr='icontains')
+    salary = django_filters.RangeFilter()
+    status = django_filters.CharFilter(lookup_expr='iexact')
+    company__name = django_filters.CharFilter(field_name='company__name', lookup_expr='icontains')
+
+    class Meta:
+        model = Job
+        fields = ['title', 'location', 'salary', 'status', 'company__name']
+
+class ApplicationFilter(django_filters.FilterSet):
+    date = django_filters.DateFromToRangeFilter(field_name='date')
+    status = django_filters.CharFilter(field_name='status', lookup_expr='icontains')
+    candidate__firstname = django_filters.CharFilter(field_name='candidate__firstname', lookup_expr='icontains')
+    candidate__lastname = django_filters.CharFilter(field_name='candidate__lastname', lookup_expr='icontains')
+    job__title = django_filters.CharFilter(field_name='job__title', lookup_expr='icontains')
+
+    class Meta:
+        model = Application
+        fields = ['date', 'status', 'candidate__firstname', 'candidate__lastname', 'job__title']
 
 class JobCreateAPIView(generics.CreateAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-
 class JobListAPIView(generics.ListAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    # filterset_fields = ["title", "description"]
+    filterset_class = JobFilter
+    search_fields = ["title", "description", "company__name"]
+
 
 
 class JobDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -40,6 +68,9 @@ class ApplicationListAPIView(generics.ListAPIView):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = ApplicationFilter
+    search_fields = ["candidate__firstname", "candidate__lastname", "job__title"]
 
 
 class ApplicationListByJobAPIView(generics.ListAPIView):
