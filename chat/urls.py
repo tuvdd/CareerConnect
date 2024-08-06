d9eba1e8f0e62cd90482cc4e9f5b9c00478268c8
Vuong Duy Duc Tu <112555576+tuvdd@users.noreply.github.com>
2024-08-06 21:39:50 +0700

    feat: chat channels WIP

--- chat/urls.py
+++ chat/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ChatRoomViewSet, MessageViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'chatrooms', ChatRoomViewSet)
 router.register(r'messages', MessageViewSet)
 
 urlpatterns = [
+    path("chat", views.index, name="index"),
+    path("chat/<str:room_name>/", views.room, name="room"),
+
     path('api/', include(router.urls)),
+    path('api/chatrooms/<int:pk>/count_unseen_messages/', ChatRoomViewSet.as_view({'get': 'count_unseen_messages'})),
+    path('api/chatrooms/get_chat_room_by_keyword/', ChatRoomViewSet.as_view({'get': 'get_chat_room_by_keyword'})),
+    path('api/chatrooms/<int:pk>/mark_messages_as_seen/', ChatRoomViewSet.as_view({'put': 'mark_messages_as_seen'})),
+    path('api/messages/<int:pk>/delete_message_if_sender/', MessageViewSet.as_view({'delete': 'delete_message_if_sender'})),
 ]
