cc9b29acc55f68c558b52bb2aed1116eca7d1352
PhucQuach1907 <phucquach1907@gmail.com>
2024-08-08 22:12:37 +0700

    fix permission

--- job/permissions.py
+++ job/permissions.py
@@ -1,21 +1,26 @@
 from rest_framework import permissions
 
+
 class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        
+
         # Kiểm tra xem user hiện tại có role là admin không
         if request.user.role == 'admin':
             return True
-        
+
         # Kiểm tra xem user hiện tại có phải là chủ sở hữu của Job hoặc Application không
+        if hasattr(obj, 'job'):
+            if obj.job.company.user == request.user:
+                return True
+
         if hasattr(obj, 'company'):
             if obj.company.user == request.user:
                 return True
-        
+
         if hasattr(obj, 'candidate'):
             if obj.candidate.user == request.user:
                 return True
-        
+
         return False
